47531ab227e5e1a85f816a210aa3fbff2758de14
unknown <salinasaniatulmabruroh@gmail.com>
2024-06-27 12:38:01 +0700

    sistempakar

--- Apps/resources/views/website/admin/editdatapenyakit.blade.php
+++ Apps/resources/views/website/admin/editdatapenyakit.blade.php
@@ -10,5 +10,8 @@
             <label for="exampleFormControlInput1">Nama Penyakit</label>
             <input type="nama" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
         </div>
-    </form>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Keterangan</label>
+            <input type="kandungan" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
 @endsection
